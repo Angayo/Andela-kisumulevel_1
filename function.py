c252289cdbc22d47e123ff243afea2e32ce13b70
Aaron Amoth <oamoth@gmail.com>
2018-06-27 18:54:46 +0300

    made changes on the python file

--- function.py
+++ function.py
@@ -1,4 +1,19 @@
+'''
+A function called sum that add two inputs
+and returns the sum of thew two inputs.
+
+function name: sum_two
+input:num_1, num_2
+output: num_1 + num_2
+
+'''
 def sum(x,y):
     ans = x+y
     return ans
-sum(10,20)
+
+num1 = int(input("Enter first number:"))
+a = num_1
+num2 = int(input("Enter second number:"))
+b = num_2
+
+print(sum(a,b))
