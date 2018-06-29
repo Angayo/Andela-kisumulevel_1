e3b42d206a325d98d9065b3c85ecdc9035805c4e
Aaron Amoth <oamoth@gmail.com>
2018-06-29 08:06:32 +0300

    changes

--- OOP/deal.py
+++ OOP/deal.py
@@ -1,36 +1,59 @@
 '''This is the place where user name is stored in the database and the status 
  if he is log in
 '''
-users = {}
-status = ""
+'''This is the place where user name is stored in the database and the status 
+ if he is log in
+'''
+from user import Users
+from db import cred_User
 
-def displayMenu():# checks if the user is registered or not
-    status = input("Are you registered user? y/n? ")
-    if status == "y":                                     
-        currentUser()
-    elif status == "n":
-        newUser()
- 
-def newUser(): #creates  new user and sees if user already exit
-    createLogin = input("Create username: ")
-    if createLogin in users:
-        print("\n Username already exist!\n")
+
+class Details():
+    def check_data():
+        command = ("Are you registered user? y/n? ")
+        while command != 'q':
+            command = input("Enter your login Details: ")
+            if command == 'y':
+                self.login()
+                print("You are logged in")
+            elif command == "n":
+                self.register()
+                print("Welcome to Andela")
+
+    def log_in(self):
+"""if the user is not registered it should 
+request the user to register. If the user."""
+        logged_in = False
+        email = str(input("Please enter your email: "))
+        password = str(input("Please enter your password: "))
+
+        fh = open("my_database", 'r')#opens a file called my_database with read priviledges
+        my_database = json.load(fh)#read a dictionary from the file
+
+    if email in my_database:
+        database_identies = my_database[email]
+        database_password = database_identies[0]
+        if database_password == password:
+            logged_in = True
+            print("You are now logged in.")
+        return logged_in
+        else:
+        print("Wrong email or password used.")
     else:
-        createPassword = input("Create password: ")
-        users[createLogin] = createPassword
-        print("\n newUser created\n")
- 
-def currentUser():#It prompts the user to enter username an password
-    login = input("Enter login name: ")
-    password = input("Enter password: ")
- 
-    if login in users and users[login] == password:
-        print("\nLogin successful!\n")
-    else:
-        print("\n User doesn't exist or wrong password!\n ")
- 
-while status != "q":
-    displayMenu()
+        print("You have not registered yet. Please register first.")
 
+    def log_out(self):
+        """Allows the user to log out of the application"""
+        if logged_in == True:
+            logged_in ==False
+            return logged_in    
 
+def make_comment(self):
+    if logged_in == True:
+    print("You can now post a comment.")
+    print("please log in first.")
 
+def reply_comment(self):
+    if logged_in == True:
+    print("You can now reply to a comment.")
+print("please log in first.")
---
+++ OOP/users.py
@@ -0,0 +1,8 @@
+class Users():
+	user_Id = 0
+	def __init__(self, email, fname, lname, password):
+		self.email = email
+		self.firstname = firstname
+		self.lastname = lastname
+		self.password = password
+		
