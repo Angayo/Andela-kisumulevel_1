cc41db4fceae78594e8372e26732923b7123986a
Aaron Amoth <oamoth@gmail.com>
2018-07-01 11:06:32 +0300

    unit test

---
+++ OOP/UnitTest/info_register.py
@@ -0,0 +1,8 @@
+class info():
+
+    def details(self):
+	    first_Name=input('Enter firstname: ')
+        username=input('Enter username: ')
+	  
+	def pass_confirm(pass1, confim_pass):
+	    return(pass1=confim_pass)
---
+++ OOP/UnitTest/login.py.py
@@ -0,0 +1,16 @@
+import unittest
+from info_register import info
+
+class Testinfo(unittest.TestCase):
+    def test_detail(self):
+        self.assertEqual(details.first_Name('aaron'),'aaron')
+        self.assertEqual(details.username('aaron'), 'aaron')
+    
+	def test_pass(self):
+	    pass1 = '123'
+		confir_pass = '123'
+		self.assertTrue(confirm_pass == pass1, true)
+	
+
+if __name__ == '__main__':
+     unittest.main()
